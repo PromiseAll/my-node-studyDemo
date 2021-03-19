82f163b0681f40216baeeeec87708904a8058f20
曾广武 <2238480780@qq.com>
2021-03-19 17:34:27 +0800

    websocket

--- 03-koa/app.js
+++ 03-koa/app.js
@@ -16,6 +16,14 @@ home.get("/", async (ctx, next) => {
   //   next();
 });
 // 装载所有子路由
+// 调用router.routes()来组装匹配好的路由，返回一个合并好的中间件
+// 调用router.allowedMethods()获得一个中间件，当发送了不符合的请求时，会返回 `405 Method Not Allowed` 或 `501 Not Implemented`
+// app.use(router.routes());
+// app.use(router.allowedMethods({ 
+//     // throw: true, // 抛出错误，代替设置响应头状态
+//     // notImplemented: () => '不支持当前请求所需要的功能',
+//     // methodNotAllowed: () => '不支持的请求方式'
+// }));
 let router = new Router();
 router.use("/home", home.routes(), home.allowedMethods());
 app.use(router.routes()).use(router.allowedMethods());
---
+++ 04-websocket/app.js
@@ -0,0 +1,58 @@
+const ws = require("nodejs-websocket");
+
+const createServer = () => {
+  let server = ws.createServer((connection) => {
+    connection.id = connection.path.replace("/", "");
+    connection.sendText("当前一共:" + server.connections.length + "人连接");
+    connection.sendMessage = sendMessage;
+
+    connection.on("text", function (result) {
+      let data = JSON.parse(result);
+      switch (data.type) {
+        case 0:
+          connection.sendText("已经接收消息");
+          break;
+        case 1:
+          connection.sendMessage(data.userId, data.message);
+          break;
+        default:
+          break;
+      }
+    });
+    connection.on("connect", function (code) {
+      console.log("开启连接", code);
+    });
+    connection.on("close", function (code) {
+      console.log("关闭连接", code);
+    });
+    connection.on("error", function (code) {
+      console.log("异常关闭", code);
+    });
+  });
+
+  return server;
+};
+
+let server = createServer();
+
+/**
+ * @description: 给指定用户发送消息
+ * @param {*} userId 指定用户id
+ * @param {*} data  发生的消息
+ */
+
+function sendMessage(userId, data) {
+  server.connections.map((receiveUser) => {
+    if (receiveUser.id == userId) {
+      receiveUser.sendText(
+        JSON.stringify({
+          sendUserId: this.id,
+          data,
+        })
+      );
+      return;
+    }
+  });
+}
+
+server.listen(3000);
